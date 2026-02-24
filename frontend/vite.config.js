b904993fc8c79676206334c732f0f5b6db268b0a
황재현 <ten02130735@gmail.com>
2026-02-24 22:46:26 +0900

    백엔드 주소 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://13.125.139.31:8089',
         changeOrigin: true,
       },
     },
